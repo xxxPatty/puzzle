f9a59ff3bca546c06995237ea897c1345a1f6ebf
Patty <L224749391@gmail.com>
2021-03-03 16:44:46 +0800

    Initial Commit

---
+++ puzzle/puzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  puzzleApp.swift
+//  puzzle
+//
+//  Created by 林湘羚 on 2021/3/3.
+//
+
+import SwiftUI
+
+@main
+struct puzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
